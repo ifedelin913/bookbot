cd88643a6973d182c836b14d1385b423e1808c91
ifedelin913 <isaiahed04@gmail.com>
2025-06-24 14:01:13 -0500

    added sys module for CLI input variability that allows different file paths to be used

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import sys
+
+# import text analysis functions from stats.py
 from stats import word_count
 from stats import char_count
 from stats import sort_char_count
@@ -9,15 +12,21 @@ def get_book_text(file):
     return contents
 
 def main():
-    # set text to book variable
-    book = get_book_text("books/frankenstein.txt")
+    # display error message if CLI format is incorrect
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    path = sys.argv[1] # path to the book is the 2nd arg in CLI input
+
+    book = get_book_text(path)
 
     # set text statistics to respective varibales
     count = word_count(book)
     char_counts = char_count(book)
     sorted_list = sort_char_count(char_counts)
 
-    #print text statistics
+    #print text statistics report
     print("============ BOOKBOT ============")
     print("Analyzing book found at books/frankenstein.txt...")
     print("----------- Word Count ----------")
